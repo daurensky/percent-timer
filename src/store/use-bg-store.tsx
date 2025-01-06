c34b2b5ab4aa197c8115ce81da0b73555c681c3c
daurensky <dkambarov17@gmail.com>
2025-01-06 07:17:12 +0500

    fix: default bg url

--- src/store/use-bg-store.tsx
+++ src/store/use-bg-store.tsx
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
 
-const defaultBg = '/bg.png'
+const defaultBg = 'bg.png'
 
 interface BgState {
   defaultBgSettled: boolean
